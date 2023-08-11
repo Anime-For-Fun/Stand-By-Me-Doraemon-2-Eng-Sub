fcdca9f10affd0dac14bcc54aac93a6a707d3a96
Siam islam <77488919+Siam-Islam@users.noreply.github.com>
2023-08-11 17:25:16 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,8 @@
  var darkColor = localStorage.getItem('dark');
- var element = document.documentElement.style;
+ var element = document.documentElement;
  if (darkColor){
-     element.setProperty("--v1", '#000');
-     element.setProperty("--v2", '#333');
-     element.setProperty("--v3", '#fff');
-     document.getElementById("sun").style.display='block';
-}
- else{
-     element.setProperty("--v1", '#f0f0f5');
-     element.setProperty("--v2", '#fff');
-     element.setProperty("--v3", '#000');
-     document.getElementById("moon").style.display='block';
-}
-
-function moonColor(){
-    document.getElementById("moon").style.display='none';
-    document.getElementById('sun').style.display='block';
-    element.setProperty("--v1", '#000');
-    element.setProperty("--v2", '#333');
-    element.setProperty("--v3", '#fff');
-    localStorage.setItem('dark', 'True');
+     element.classList.add('dark-mode');
  }
-
-function sunColor(){
-    document.getElementById("sun").style.display='none';
-    document.getElementById('moon').style.display='block';
-    element.setProperty("--v1", '#f0f0f5');
-    element.setProperty("--v2", '#fff');
-    element.setProperty("--v3", '#000');
-    if (darkColor){
-    localStorage.clear();}
-    }
- 
-
+ else{
+     element.classList.add('sun-mode');
+}
