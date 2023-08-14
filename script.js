2eda17d4a44f3913ee02d31eb80087fe2114b32a
Siam islam <77488919+Siam-Islam@users.noreply.github.com>
2023-08-14 12:07:00 +0600

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,17 @@
  else{
      element.classList.add('sun-mode');
 }
+
+function moonColor(){
+    element.classList.remove("sun-mode");
+    element.classList.add("dark-mode");
+    localStorage.setItem("dark", "true");
+}
+
+function sunColor(){
+    element.classList.remove("dark-mode");
+    element.classList.add("sun-mode");
+    if (darkColor){
+    localStorage.clear();}
+    }
+
